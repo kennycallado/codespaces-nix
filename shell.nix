3f5449d92a67f2dbc68bc9d6d99ba562cc4c561f
kennycallado <kennycallado@hotmail.com>
2024-04-26 12:30:40 +0200

    UPDATE

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz") { } }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.11.tar.gz") { } }:
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     # needed by nix
     nixpkgs-fmt
     rnix-lsp
+
+    # development tools
+    neovim
+    curl
+    git
   ];
 }
