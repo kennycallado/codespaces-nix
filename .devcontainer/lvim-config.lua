bc6637efe6cbbdf85b3676c159bcd4882a905cad
kennycallado <kennycallado@hotmail.com>
2024-04-26 13:02:27 +0200

    UPDATE

---
+++ .devcontainer/lvim-config.lua
@@ -0,0 +1,11 @@
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.relativenumber = true
+vim.opt.spelllang = "es"
+vim.opt.scrolloff = 4
+
+lvim.transparent_window = true
+lvim.builtin.nvimtree.setup.view.side = "right"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+lvim.builtin.terminal.open_mapping = "<C-;>"
